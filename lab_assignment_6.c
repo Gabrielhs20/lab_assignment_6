3c8775bd1d837946d7d63477aa611664ac26d53b
Gabriel Hernandez <gabo20hernandez@gmail.com>
2023-02-22 17:02:26 -0500

    Lab assignment6 finished

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
+	int index = -1;
+    if(low <= high){
+        int mid = (low + high) / 2;
+
+        if(numbers[mid] == value){
+            return mid;
+        }
+        else if(numbers[mid] < value){
+            return search(numbers,mid+1, high, value);
+        }
+        else{
+            return search(numbers, low, mid - 1, value);
+        }
+    }
 	return -1;
 }
 
